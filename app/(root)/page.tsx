75a2539dacdade6b19278c57a055970d7f71ecba
Jefferson E <jeffersonsimon6@gmail.com>
2024-09-21 23:21:54 +0530

    added header and balance box |  components
    Props, imports, packages

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,29 @@
-import React from 'react'
+import HeaderBox from "@/components/HeaderBox";
+import TotalBalanceBox from "@/components/TotalBalanceBox";
+import React from "react";
 
 const Home = () => {
+  const loggendIn = { firstname: "Jefferson" };
   return (
-    <div>Home</div>
-  )
-}
+    <section className="home">
+      <div className="home-content">
+        <header className="home-header">
+          <HeaderBox
+            type="greeting"
+            title="Welcome"
+            user={loggendIn?.firstname || "Guest"}
+            subtext="Access and manage your transactions efficiently."
+          />
 
-export default Home
+          <TotalBalanceBox 
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35}
+          />
+        </header>
+      </div>
+    </section>
+  );
+};
+
+export default Home;
