98cc1aee2452e8638b7e393289daa75ce75acc0b
Jefferson E <jeffersonsimon6@gmail.com>
2024-09-22 17:52:18 +0530

    sidebar component

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,12 +1,15 @@
+import Sidebar from "@/components/Sidebar";
 
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  const loggedIn = { firstName: 'Jefferson', lastName: 'E'};
+
   return (
-   <main>
-    SIDEBAR
+   <main className="flex h-screen w-full font-inter">
+    <Sidebar user={loggedIn}/>
     {children}
    </main>
   );
